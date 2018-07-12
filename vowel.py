904baf4c9f784b7ed6d01973f01d787ab4e51178
Padmasaranyav <41141605+Padmasaranyav@users.noreply.github.com>
2018-07-12 15:11:16 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,10 @@
+character=input()
+l=['a','e','i','o','u']
+if(ord(character)>=97 and ord(character)<=122))
+  if character in l:
+    print("Vowel")
+   else:
+    print("Consonant")
+else:
+   print("invalid")
+   
